d9b603983470810540d8cb5ec11311c5125341c9
Gustav Lokna <loknagustav@gmail.com>
2025-10-27 13:48:25 +0100

    tetser 2

--- src/train/svc.py
+++ src/train/svc.py
@@ -185,7 +185,7 @@ def SVC_tetsing(config, file_path):
 
         subjects = np.unique(subject)
         subj_results = []
-        """"
+        
         for subj in subjects:
             train_mask = np.array([subj not in str(s) for s in subject]) # written this difficulte to have the opertunity to exclude synthetic samples
             test_mask = np.array([str(s) == str(subj) for s in subject])
@@ -246,15 +246,7 @@ def SVC_tetsing(config, file_path):
 
         all_results.extend(subj_results)
         all_results.append(avg_row)
-        """
-        avg_row = {
-            "model_type": model_type,
-            "label_map": "1,2,3",
-            "subject": "average",
-            "baseline_acc": base_acc,
-            "baseline_recall":base_recall,
-            "baseline_kappa": base_kappa,
-        }
+
 
         all_results.append(avg_row)
     # name output file after input npz
