ee2f21064c79d18489969bd045146f28a0ca852b
Gustav Lokna <loknagustav@gmail.com>
2025-10-29 14:29:18 +0100

    tetser 2

--- src/train/csp_multimodel.py
+++ src/train/csp_multimodel.py
@@ -192,6 +192,6 @@ def test_csp_models_raw(config, file_path, n_csp_components=256):
             all_results.append(avg_row)
 
     input_name = os.path.splitext(os.path.basename(file_path))[0]
-    output_path = f"model_eval/csp/sk_models_loso_eval_{input_name}_n_components_{n_csp_components}.xlsx"
+    output_path = f"model_eval/csp/csp_{input_name}_with_n_csp_components_{n_csp_components}.xlsx"
     pd.DataFrame(all_results).to_excel(output_path, index=False)
     print(f"\nResults saved to {output_path}")
